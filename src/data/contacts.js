1b905a38146bb9f65cea912e7f1d02e710c3dd89
Dona Anda <ceka_dona@yahoo.com>
2018-05-01 08:17:16 -0700

    forms-inputs - Dona Anda

---
+++ src/data/contacts.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        firstName: 'Don',
+        lastName: 'Nuk',
+        phone: '9493949078',
+        email: 'yes@yahoo.com'
+    },
+    {
+        firstName: 'Dike',
+        lastName: 'Lop',
+        phone: '9495949068',
+        email: 'no@yahoo.com'
+    },
+    {
+        firstName: 'Tike',
+        lastName: 'Luk',
+        phone: '9495949067',
+        email: 'wesoscrewed@gmail.com'
+    },
+    {
+        firstName: 'Dige',
+        lastName: 'Loli',
+        phone: '9495849067',
+        email: 'gg@yahoo.com'
+    }
+];
